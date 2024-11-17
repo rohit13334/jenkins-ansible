f60c6025148288034cc5b480a6022db93f1e55c5
rohit13334 <68186886+rohit13334@users.noreply.github.com>
2024-11-17 22:02:20 +0530

    Update run_ansible_playbook.sh

--- scripts/run_ansible_playbook.sh
+++ scripts/run_ansible_playbook.sh
@@ -11,4 +11,4 @@ PLAYBOOK_PATH="./ansible_playbooks/splunkinstall.yml"
 ANSIBLE_USER="ansible"
 # Run the ansible-playbook command with the target_host variable
 # ansible-playbook ${PLAYBOOK_PATH} --extra-vars "target_host=${TARGET_HOST}" --verbose
-sudo -u $ANSIBLE_USER ansible-playbook ${PLAYBOOK_PATH} extra-vars "target_host=${TARGET_HOST}" --verbose
+sudo -u $ANSIBLE_USER ansible-playbook ${PLAYBOOK_PATH} --verbose
