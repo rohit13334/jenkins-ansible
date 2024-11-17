835ff9326993ee3df4611327f80312e87827ee36
rohit13334 <68186886+rohit13334@users.noreply.github.com>
2024-11-17 20:42:38 +0530

    Create run_ansible_playbook.sh
    Script changes done

---
+++ scripts/run_ansible_playbook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Fail on any error
+set -e
+
+# Variables (You can pass target_host as a parameter to this script)
+TARGET_HOST=$1
+
+# Path to Ansible playbook
+PLAYBOOK_PATH="./ansible_playbooks/splunk2.yml"
+
+# Run the ansible-playbook command with the target_host variable
+ansible-playbook ${PLAYBOOK_PATH} --extra-vars "target_host=${TARGET_HOST}" --verbose
