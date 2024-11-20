e7677c0bf6ead4e26b0eeded0a532f900f6ebf91
rohit13334 <68186886+rohit13334@users.noreply.github.com>
2024-11-20 18:44:59 +0530

    Update run_ansible_playbook.sh

--- scripts/run_ansible_playbook.sh
+++ scripts/run_ansible_playbook.sh
@@ -4,11 +4,12 @@
 set -e
 
 # Variables (You can pass target_host as a parameter to this script)
-TARGET_HOST=$1
+TARGET_HOSTNAME=$1
 PLAY_NAME=$2
 # Path to Ansible playbook
 PLAYBOOK_PATH="./ansible_playbooks/${PLAY_NAME}"
 ANSIBLE_USER="ansible"
+echo "target hosts are ${TARGET_HOSTNAME} "
 # Run the ansible-playbook command with the target_host variable
 # ansible-playbook ${PLAYBOOK_PATH} --extra-vars "target_host=${TARGET_HOST}" --verbose
-sudo -u $ANSIBLE_USER ansible-playbook ${PLAYBOOK_PATH} -i ${TARGET_HOST}--verbose
+sudo -u $ANSIBLE_USER ansible-playbook ${PLAYBOOK_PATH} -i ${TARGET_HOSTNAME} --verbose
