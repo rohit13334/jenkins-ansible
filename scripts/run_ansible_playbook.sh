9ef1a4f814620ac0eb2a0a370cfa0068e676ca7f
rohit13334 <68186886+rohit13334@users.noreply.github.com>
2024-11-20 17:59:52 +0530

    Update run_ansible_playbook.sh

--- scripts/run_ansible_playbook.sh
+++ scripts/run_ansible_playbook.sh
@@ -5,10 +5,10 @@ set -e
 
 # Variables (You can pass target_host as a parameter to this script)
 TARGET_HOST=$1
-
+PLAY_NAME=$2
 # Path to Ansible playbook
-PLAYBOOK_PATH="./ansible_playbooks/splunkinstall.yml"
+PLAYBOOK_PATH="./ansible_playbooks/${PLAY_NAME}"
 ANSIBLE_USER="ansible"
 # Run the ansible-playbook command with the target_host variable
 # ansible-playbook ${PLAYBOOK_PATH} --extra-vars "target_host=${TARGET_HOST}" --verbose
-sudo -u $ANSIBLE_USER ansible-playbook ${PLAYBOOK_PATH} --verbose
+sudo -u $ANSIBLE_USER ansible-playbook ${PLAYBOOK_PATH} -i ${TARGET_HOST}--verbose
